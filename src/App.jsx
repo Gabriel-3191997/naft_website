d8ac2eb1608f2e26baedb3e19d5498f10dd1d785
Gabriel_31997 <gabrielwkun@gmail.com>
2026-01-20 14:12:21 +0000

    meta description update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
-import './style.scss'
-import TopNav from './components/topbar'
-import NavBar from './components/navbar'
-import Advertise from './components/advertise'
-import HeroSection from './components/hero-image'
-import Section from './components/section'
+import React from 'react';
+import './style.scss';
+import './App.css';
+import NavBar from './components/navbar';
+import Advertise from './components/advertise';
+import HeroSection from './components/hero-image';
+import Section from './components/section';
 import Footer from './components/footer';
-
-import './App.css'
-
 
 class App extends React.Component {
   render() {
-    return(
+    return (
       <>
-    <div className="bg-white fixed top-0 z-50 w-full">
-        <NavBar/>
-          <Advertise/>
-      {/*<TopNav/>*/}
-    </div>
-    <div>
-      <HeroSection/>
-      <Section/>
-      <Footer/>
-    </div>
-    </>
+        {/* Top fixed nav */}
+        <div className="bg-white fixed top-0 z-50 w-full">
+          <NavBar />
+          <Advertise />
+        </div>
+
+        {/* Main content */}
+        <div className="pt-[100px]">
+          <HeroSection />
+          <Section />
+          <Footer />
+        </div>
+      </>
     );
   }
 }
-
-
 
 export default App;
