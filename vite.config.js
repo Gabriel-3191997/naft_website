72f8dc97e5141ddad3fc567ba9b5ae01d1f7c18e
Gabriel_31997 <gabrielwkun@gmail.com>
2026-01-20 10:06:17 +0000

    vite config modified

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host: true, // allows access from LAN
-    port: 5173, // specify port
-  },
-  base: './',
   plugins: [
     react(),
     tailwindcss(),
